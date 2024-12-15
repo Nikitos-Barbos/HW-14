8b4fc35eba7bdbf37eaa712f597b3f93221bcf7a
Nikitos-Barbos <sobemov@gmail.com>
2024-12-15 20:38:12 +0300

    HW-14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author author1 = new Author("Максим", "Бивнев");
+        Author author2 = new Author("Данте", "Протрезвелов");
+
+        Book book1 = new Book(author1, 1999, "Забыть вчера, как завтра");
+        Book book2 = new Book(author2, 1997, "В тишине и покое");
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+        book1.setYear(1950);
+
+        System.out.println("Updated book information:");
+        System.out.println(book1);
     }
 }
